a7ff6379ebcef8d86f07b5fcd33c48a4ad92946b
spencergamero23 <bluerayman23@gmail.com>
2025-09-24 14:15:07 -0700

    Small update
    small update being added

--- Car-website/src/main.jsx
+++ Car-website/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
+
 import * as THREE from 'three';
 const scene = new THREE.Scene();
 
@@ -18,10 +20,24 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
+
+scene.background  = new THREE.Color(0x000000);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
@@ -29,8 +45,11 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
+  controls.update();
   renderer.render(scene, camera);
 }
+
+
 
 animate();
 createRoot(document.getElementById('root')).render(

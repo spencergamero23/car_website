0ca78ecf8b63e4a4b88d7ef4eada3ef929a79f37
spencergamero23 <bluerayman23@gmail.com>
2025-09-30 14:58:51 -0700

    Playing around with this

--- Car-website/src/main.jsx
+++ Car-website/src/main.jsx
@@ -8,7 +8,7 @@ import { OrbitControls } from 'three/examples/jsm/Addons.js'
 import * as THREE from 'three';
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
@@ -20,7 +20,7 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
+const material = new THREE.MeshPhysicalMaterial({ color: 0xff6347, clearcoat: 1, clearcoatRoughness: 1, metalness: 0.5, roughness: 0.0 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
@@ -28,9 +28,9 @@ scene.add(torus);
 scene.background  = new THREE.Color(0x000000);
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
+pointLight.position.set(15, 15, 15);
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
+const ambientLight = new THREE.AmbientLight(0xffffff,3);
 scene.add(pointLight, ambientLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
